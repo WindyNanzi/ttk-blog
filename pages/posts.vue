3ab6823c2b549e2921704017151af32918db48c4
艾伦耶格尔 <39194294+WindyNanzi@users.noreply.github.com>
2025-01-30 14:39:39 +0800

    chore: 样式微调

--- pages/posts.vue
+++ pages/posts.vue
@@ -60,7 +60,39 @@ const selectPost = (path) => {
   flex: 1;
   padding: 20px;
   overflow-y: auto;
+  overflow-x: hidden; /* 删除横向滚动条 */
   background-color: #fff;
   margin-left: 10px;
 }
+
+/* 滚动条样式 */
+.sidebar::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+.sidebar::-webkit-scrollbar-thumb {
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+}
+
+.sidebar::-webkit-scrollbar-thumb:hover {
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.content::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+
+.content::-webkit-scrollbar-thumb {
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+}
+
+.content::-webkit-scrollbar-thumb:hover {
+  background: rgba(0, 0, 0, 0.5);
+}
+
 </style>
