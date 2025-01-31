9a05066afe0aa41e3818abb37c0eaca57c2a5ac8
艾伦耶格尔 <39194294+WindyNanzi@users.noreply.github.com>
2025-01-31 11:31:54 +0800

    feat: 添加 no-referrer 设置
    - 在 nuxt.config.ts 中添加 app 配置
    - 在 app.head.meta 中添加 referrer 设置为 no-referrer
    - 此设置可以增强隐私保护，防止 referrer 信息泄露

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   extends:['@nuxt-themes/docus'], // 使用 docus 主题
+  app: {
+    head: {
+      meta:[
+        { name: 'referrer', content: 'no-referrer' }
+      ]
+    }
+  }
 })
